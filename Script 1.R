07ed2cb8bc320e49a18819e46e01f6ddf2a5473f
Francesca Naudi <f.naudi@student.rug.nl>
2025-11-25 15:34:32 +0100

    .

---
+++ Script 1.R
@@ -0,0 +1,10 @@
+#How does individual variation in timing relative to caterpillar food peaks affect reproductive performance and its repeatability in pied flycatchers
+
+#q1.1 Do pied flycatchers adjust their arrival and laying dates to match annual variation in caterpillar peak date? and are these dates repeatable? 
+#firs model between and within individuals for timing and caterpillar peak dates 
+#repeatability plus plastic response - whether they adjust between years
+#laying date 0 simplify by using females only
+
+#q1.2 looking at mismatch from peak date and seeing if there is a trend among individuals and looking at what success outcomes we find (clutch size & recruits, maybe mass too? ) 
+#mismatch becomes predictor variable and can look at within birds whether in years they are more matched they have a higher fitness. (clutch size and recruits will have same data for males and females  since they breed together, so maybe best to select females in second step.)
+#either looka t data separately or put data together and make sex a fixed factor).
